348a014e0814d52b2669da8e4f00d783300bb3f6
3jAssociation <126234030+3jAssociation@users.noreply.github.com>
2024-12-05 13:38:40 +0530

    Temporarily hide address in Location.js UAE
    Temporarily hide address in Location.js

--- src/components/ContactUs/Location/Location.js
+++ src/components/ContactUs/Location/Location.js
@@ -27,15 +27,15 @@ const Location = () => {
     //     "https://www.google.com/maps/place/3J+Associate+Technology+Services/@23.1289437,79.8797406,18.29z/data=!4m14!1m7!3m6!1s0x3981b3a7a741aaf3:0x369322ccbae5d977!2s3J+Associate+Technology+Service!8m2!3d23.1293316!4d79.8806731!16s%2Fg%2F11pgkcf2xm!3m5!1s0x3981b398f1fd4c85:0x5dd9b396a36de953!8m2!3d23.1297222!4d79.8810364!16s%2Fg%2F11n2h9376v",
     //   image: MapImg2,
     // },
-    {
-      Primary: "Abu Dhabi, UAE",
-      Secondary: "Building 22, Unit No. 003 Al Reef Down Town,Abu Dhabi. ",
-      phone: "Phone: +971-0529041605",
-      Email: "Email: info@blitzpath.in",
-      location:
-        "https://www.google.com/maps/place/Al+Reef+Downtown/@24.4570255,54.6732983,17z/data=!3m1!4b1!4m6!3m5!1s0x3e5e49476b5a77a7:0x974eadceb4928af8!8m2!3d24.4570255!4d54.6732983!16s%2Fg%2F11f61sy13x",
-      image: MapImg3,
-    },
+   // {
+     // Primary: "Abu Dhabi, UAE",
+      //Secondary: "Building 22, Unit No. 003 Al Reef Down Town,Abu Dhabi. ",
+      //phone: "Phone: +971-0529041605",
+      //Email: "Email: info@blitzpath.in",
+      //location:
+       // "https://www.google.com/maps/place/Al+Reef+Downtown/@24.4570255,54.6732983,17z/data=!3m1!4b1!4m6!3m5!1s0x3e5e49476b5a77a7:0x974eadceb4928af8!8m2!3d24.4570255!4d54.6732983!16s%2Fg%2F11f61sy13x",
+     // image: MapImg3,
+   // },
   ];
   useEffect(() => {
     window.scrollTo(0, 0);
